6bdf689262f693bf6d990d02958ddf5a92aa0325
Santiago Avila <xavier0823@gmail.com>
2022-05-04 20:18:10 -0500

    Interaccion

--- Clase01Sintaxis/04Ejercicio01/javascript/main.js
+++ Clase01Sintaxis/04Ejercicio01/javascript/main.js
@@ -4,10 +4,16 @@
 
     "SUMA"
    
-    en una variable y los números 3 y 5 en otra variable. 
+    en una variable y los números 3 y 5 en otras variables. 
 
     Finalmente, imprima en consola el resultado de la operación usando las variables 
     donde tiene almacenados el 3 y el 5. 
 
     
  */
+
+    
+    let operacion = "SUMA";
+    let num1 = 3;
+    let num2 = 5;
+    console.log(operacion, num1+num2);
--- Clase01Sintaxis/05Interaccion/javascript/main.js
+++ Clase01Sintaxis/05Interaccion/javascript/main.js
@@ -1,8 +1,12 @@
 
 // console.log
-
-
-//prompt
-
+console.log("HOLA", 1, 2, false, 1.2, null, undefined);
 
 //alert
+alert("ESCRIBO UN MENSAJE");
+alert(100);
+alert(true);
+
+//prompt
+let apellido = prompt("Escriba su apellido");
+alert(apellido);
