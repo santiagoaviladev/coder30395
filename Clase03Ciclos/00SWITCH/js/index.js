5d82a5ec66c59fac676f1bc8c86a809ae024efd0
Santiago Avila <xavier0823@gmail.com>
2022-05-11 11:08:00 -0500

    ÂClase Ciclos Template

---
+++ Clase03Ciclos/00SWITCH/js/index.js
@@ -0,0 +1,57 @@
+    /*
+        Escriba un programa que pida al usuario:
+        Una operacion: SUMA / MUL 
+        2 números
+        e imprima en consola, el resultado de la operación.
+        
+        Complete el ejercicio, con DIVISION y RESTA. 
+
+        SI TRATA DE DIVIDIR POR 0; MUESTRE UN ERROR
+
+    */ 
+
+
+    const operacion = prompt(`INGRESE UNA OPERACION: 
+        SUMA 
+        MUL
+        DIV
+        RESTA`);
+
+    const numero1 = Number(prompt("Ingrese un número"));
+    const numero2 = Number(prompt("Ingrese otro número"));
+
+    if(isNaN(numero1) || isNaN(numero2))
+    {
+        alert("NUMEROS NO VALIDOS")
+    }
+    else
+    {
+        if(operacion==="SUMA")
+        {
+           console.log("LA SUMA ES: "+ (numero1+numero2));
+         }
+         else if(operacion==="MUL")
+         {
+            console.log("LA MUL ES: "+ (numero1*numero2));
+         }
+        else if(operacion==="RESTA")
+        {
+            console.log("LA RESTA ES: "+ (numero1-numero2));
+        }
+        else if(operacion==="DIV")
+        {
+            if(numero2===0)
+            {
+                 alert("NO PUEDO DIVIDIR POR 0");
+            }
+        else
+        {
+            console.log("LA DIV ES: "+ (numero1/numero2));
+        }
+
+    }
+        else 
+        {
+            console.log("OPERACION INVÁLIDA");
+        }
+    }
---
+++ Clase03Ciclos/04EJERCICIO01/js/index.js
@@ -0,0 +1,13 @@
+/*
+ Escriba un programa que muestre la configuración inicial de un tablero de ajedrez: 
+
+ TCARRACT
+ PPPPPPPP
+ 00000000
+ 00000000
+ 00000000
+ 00000000
+ PPPPPPPP
+ TCARRACT
+
+*/
