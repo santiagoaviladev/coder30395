33135002cff02a83d65d03451a0b7c9eecdec8a2
Santiago Avila <xavier0823@gmail.com>
2022-05-11 12:02:49 -0500

    Find the bug

--- Clase03Ciclos/05EJERCICIO02/js/index.js
+++ Clase03Ciclos/05EJERCICIO02/js/index.js
@@ -1,10 +1,19 @@
 /*
- Escriba un programa que calcule el factorial de un número ingresado por el usuario
 
- El factorial está definido como: 
+ Usted ha sido contratado para hacer un programa de gestión 
+ de precios de una bodega. 
 
- fact(n) = n*(n-1)*(n-2)*(n-3)*.... *1
+ Escriba un programa que pida al usuario el número de paquetes 
+ que ingresaron en el día. 
 
- 
+ Y el peso de cada uno de los paquetes.
+
+ Y Calcule (mostrar en consola o en un alert)
+ * El mayor peso
+ * El menor peso
+ * El peso promedio de los paquetes
+
+
+
 
 */
