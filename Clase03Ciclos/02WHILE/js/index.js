0e10d42c3429997824c755a44e4db1a4f436aba4
Santiago Avila <xavier0823@gmail.com>
2022-05-11 19:59:24 -0500

    while

---
+++ Clase03Ciclos/02WHILE/js/index.js
@@ -0,0 +1,54 @@
+
+  let i=1;
+
+  while(i<=10)
+  {
+      console.log(i);
+      i++;
+  }
+
+  // Solo Pares
+  console.log("SOLO PARES VERSION 1")
+  
+  let j=0;
+
+  while(j<=10)
+  {
+      console.log(j);
+      j+=2;
+  }
+
+  // Solo Pares
+  console.log("SOLO PARES VERSION 2")
+  
+  let k=0;
+
+  while(k<=10)
+  {
+      if(k%2===0)
+      {
+         console.log(k);
+      }
+      k++;
+  }
+
+console.log("SOLO IMPARES v1")
+let r=1;
+while(r<=10)
+{
+    if(r%2!==0) // % = MODULO (resiudo de la división)
+    {
+        console.log(r);
+    }   
+    r++;
+}
+
+console.log("SOLO IMPARES v2")
+let q=1;
+while(q<=10)
+{
+   
+    console.log(q);
+    q+=2;
+}
+  
--- Clase03Ciclos/04EJERCICIO01/js/index.js
+++ Clase03Ciclos/04EJERCICIO01/js/index.js
@@ -1,6 +1,6 @@
 /*
- Escriba un programa que muestre la configuración inicial de un tablero de ajedrez: 
-
+ Escriba un programa que muestre 
+ la configuración inicial de un tablero de ajedrez: 
  TCARRACT
  PPPPPPPP
  00000000
@@ -9,5 +9,20 @@
  00000000
  PPPPPPPP
  TCARRACT
-
 */
+
+    for(let linea=1;linea<=8;linea++)
+    {
+        if(linea===1 || linea===8)
+        {
+            console.log("TCARRACT");
+        }
+        else if(linea ===2 || linea===7)
+        {
+            console.log("PPPPPPPP");
+        }
+        else
+        {
+             console.log("00000000");
+        }
+    }
