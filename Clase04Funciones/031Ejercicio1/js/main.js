219accaf39cf515defb56bf41e9e1ba1dd3dd7fd
Santiago Avila <xavier0823@gmail.com>
2022-05-16 16:49:45 -0500

    Clase Funciones tempmlate

---
+++ Clase04Funciones/031Ejercicio1/js/main.js
@@ -0,0 +1,14 @@
+
+ /*Escriba un programa que permita simular una calculadora. 
+ El usuario debe ingresar una de las siguientes opciones: 
+ 1. Suma 
+ 2. Resta
+ 3. Mul 
+ 4. Div 
+ 5. Fin
+ El menú debe mostrarse, siempre que el usuario no ingrese la opción 5
+ El programa debe mostrar error en los siguientes casos: 
+    A. El usuario no selecciona una opción válida de 1 a 5 
+    B. El usuario no ingresa datos válidos para operar
+    C. El usuario ingresa un '0' como segundo operando para la división
+*/
---
+++ Clase04Funciones/061Ejercicio2/js/main.js
@@ -0,0 +1,28 @@
+
+/*
+    Máquina de Café y Mate
+    Escriba un programa que simule una máquina dispensadora de café y mate
+    El mate cuesta 3 pesos
+    EL café cuesta 2 pesos
+    La máquina debe mostrar un menú, con los productos disponibles y los precios
+    Igualmente, debe mostrar una opción de 'Salir' 
+    El menú debe repetirse siempre que el usuario no seleccione la opción de salir
+
+    Una vez el usuario ha seleccionado el producto que quiere, 
+    el sistema debe pedir que ingrese un monto de dinero
+
+    Si el usuario ingresa un monto mayor, el sistema debe mostrar el cambio entregado 
+    Si el usuario ingresa un monto menot, el sistema debe mostrar de nuevo la opción para ingresar dinero
+
+
+    La máquina solo acepta dinero en denominaciones de: 
+    1,2, 5 y 10 pesos. 
+    
+    El sistema debe mostrar errores cuando: 
+
+    * El usuario ingresa una opción no válida en el menú
+    * El uusuario ingresa un monto no numérico 
+    * El usuario ingresa un monto no aceptado por la máquina
+    
+
+*/
