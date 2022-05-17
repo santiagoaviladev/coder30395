74ac1895d3e25ba1ec252653b6ad99b059ff1954
Santiago Avila <xavier0823@gmail.com>
2022-05-16 19:14:32 -0500

    clase funciones

--- Clase04Funciones/02FuncionConParametros/js/main.js
+++ Clase04Funciones/02FuncionConParametros/js/main.js
@@ -1,2 +1,14 @@
 
- //funcion simple
+ //funcion con parámetros
+
+  function sumar(a,b)
+  {
+    console.log(`SUMA ES ${a+b}`);
+  }
+
+   const operando1 = Number(prompt("Ingrese un num"));
+   const operando2 = Number(prompt("Ingrese otro num"));
+
+  sumar(operando1, operando2);
+
+  sumar("HOLA", "CACEROLA");
