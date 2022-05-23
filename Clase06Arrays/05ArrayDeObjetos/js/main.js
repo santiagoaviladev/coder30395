1aec0148ab801679409b94d6cf782b9ee3f1f4b3
Santiago Avila <xavier0823@gmail.com>
2022-05-23 16:53:48 -0500

    Arrays

---
+++ Clase06Arrays/05ArrayDeObjetos/js/main.js
@@ -0,0 +1,32 @@
+ 
+  const productos = [
+    {
+        id:1,
+        nombre:"leche",
+        precio:2000,
+    },
+    {
+        id:2,
+        nombre:"cerveza",
+        precio:3000,
+
+    },
+    {
+        id:3,
+        nombre:"pan",
+        precio:200,
+
+    }
+
+  ]
+
+  console.log(productos);
+
+  for(let i = 0;i<productos.length;i++)
+  {
+      console.log(productos[i]);
+  }
+
+  //Recorrer con of
+  console.log("Recorrer con OF")
+  
---
+++ Clase06Arrays/06Ejericio02/js/main.js
@@ -0,0 +1,8 @@
+/* Agrege al sistema para JUGAR UNO: 
+
+ 1. Una opción para que el sistema cree los jugadores de forma aleatoria
+    Un jugador debe tener: id, nickname, cartasEnMano
+ 2. Una opción para borrar un jugador dado su id
+ 3. Una opción para que el sistema reparta las cartas (7 para cada Jugador)
+ 4. Una clase para que manejar el estado del juego
+    */
