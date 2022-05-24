92c93c47926cebef48da2dc581252defd2a36631
Santiago Avila <xavier0823@gmail.com>
2022-05-23 20:34:09 -0500

    Comentarios en funciones

--- Clase06Arrays/03PropiedadesYMetodos/js/main.js
+++ Clase06Arrays/03PropiedadesYMetodos/js/main.js
@@ -11,10 +11,12 @@ const misNumeros = [10,9,8,7,6,6,100,200,300,2000,-100,400, 500];
  }
 
 //push
+// agrega un elemento al final del array
  misNumeros.push(1000);
  console.log("NUMEROS", misNumeros);
 
 //unshift
+// agrega un elemento al inicio del array
  misNumeros.unshift(-1000);
  console.log("NUMEROS", misNumeros);
 
@@ -33,31 +35,38 @@ const misNumeros = [10,9,8,7,6,6,100,200,300,2000,-100,400, 500];
 */ 
 
 //pop
+// elimina el último elemento del array
     misNumeros.pop();
     console.log("NUMEROS SIN ULTIMO", misNumeros);
 
 //shift
+// elimina el primer elemento del array 
     misNumeros.shift();
     console.log("NUMEROS SIN ULTIMO", misNumeros);
 
 
 
 //splice
+// Elimina elementos del array desde el índice dado, 
+//con la longitud dada
  const perros = ["budhis", "firulais", "silver", "lucy"];
  perros.splice(1,2);
  console.log(perros);
 
 
 //join
+// Une los elementos del array, con el caracter dado
 const gatos = ["loki", "michi", "ursula", "zuko", "pola"];
 const texto = gatos.join(" ");
 console.log(texto);
 
 //concat
+// Une 2 arrays
 const mascotas = perros.concat(gatos);
 console.log(mascotas);
 
 //slice
+// retorna un fragmento del array // NO MODIFICA EL ARRAY ORIGINA
 const otrosPerros = ["budhis", "firulais", "silver", "lucy"];
 const nuevoPerros= otrosPerros.slice(1,2);
 console.log(nuevoPerros);
@@ -66,6 +75,7 @@ console.log(nuevoPerros);
 
 //indexOf
 let mascotaParaBuscar = prompt("Ingrese su mascota");
+// Busca un elemento en el array y devuelve el índice
 const indice = otrosPerros.indexOf(mascotaParaBuscar);
 if(indice===-1)
 {
@@ -86,7 +96,6 @@ if(esta)
 else{
     alert("SU mascota  está con nosotros");
 }
-
 
 //reverse
 mascotas.reverse(); // invierte el array
