0c50c290a7d33ba6013f13643691db14f139f5e2
Santiago Avila <xavier0823@gmail.com>
2022-05-25 20:26:32 -0500

    HOF

--- Clase07HOF/00Abstraccion/js/main.js
+++ Clase07HOF/00Abstraccion/js/main.js
@@ -1,8 +1,30 @@
 
+   console.log("UNO", "o", "VARIAS", "CADENAS", "DE", "TEXTO");
+
+
  //Función que retorna función
+ function mayorQue(n)
+ {
+        return (m) =>  m>n ;
+ }
+
+  const mayorquediez = mayorQue(10);
+ 
+  console.log(mayorquediez(5)); // false;
 
 
 
  //Función que recibe función por parámetro
   
-   
+   function porCadaUno(arr, fun)
+   {
+       //for(let i=0;i<arr.length;i++)
+       for(const elemento of arr)
+       {
+            fun(elemento);
+       }
+   }
+
+     porCadaUno(["HOLa", "cacerola", "cómo", "estas"], console.log);
+
+    
--- Clase07HOF/01CRUDUSUARIOS/js/main.js
+++ Clase07HOF/01CRUDUSUARIOS/js/main.js
@@ -1,5 +1,11 @@
-// SISTEMA CRUD USUARIOS
- 
+/* 
+    SISTEMA CRUD USUARIOS
+    Create 
+    Read
+    Update
+    Delete 
+
+ */
  class Usuario{
      constructor(id,nombre,apellido)
      {
@@ -9,12 +15,13 @@
      }
  }
 
- const usuario1 = new Usuario(1, "Ezequiel", "Natale");
- const usuario2 = new Usuario(2, "Roman", "Stoleru");
- const usuario3 = new Usuario(3, "Fernando", "Benitez");
- const usuario4 = new Usuario(4, "Jorge", "Dáttoli");
- const usuario5 = new Usuario(5, "Ivana", "Villanueva");
- const usuario6 = new Usuario(6, "Fabian", "Gutman");
+ const usuario1 = new Usuario(1, "Natalia", "Chiara");
+ const usuario2 = new Usuario(2, "Diego", "Del Hoyo");
+ const usuario3 = new Usuario(3, "Alejandro", "Lombardi");
+ const usuario4 = new Usuario(4, "Diego", "Castro");
+ const usuario5 = new Usuario(5, "Gabriel", "Odirozola");
+ const usuario6 = new Usuario(6, "Juan", "Ferrari");
+
 
  const usuarios = [usuario1,usuario2,usuario3, usuario4, usuario5, usuario6];
  console.log("INICIAL:", usuarios);
@@ -34,7 +41,7 @@
                             3. Modificar Usuario
                             4. Listar usuarios
                             5. Buscar Usuario
-                            6. Listar NOMBRE + APELLIDO
+                            6. Filtrar Listado
                             10. Salir`));
 
     switch(opcion)
@@ -47,10 +54,12 @@
         case 2: 
         {
             eliminarUsuario();
+            break;
         }
         case 3: 
         {
             modificarUsuario();
+            break;
         }
         case 4:
         {
@@ -59,12 +68,33 @@
         }
         case 5:
         {
-                buscarUsuario();
+               const encontrado= buscarUsuario();
+
+               if(encontrado)
+               {
+                   alert(`El usuario es: 
+                          ID: ${encontrado.id}
+                          NOMBRE: ${encontrado.nombre}
+                          APELLIDO: ${encontrado.apellido}`)
+               }
+               else
+               {
+                   alert("NO LO ENCONTRÉ");
+               }
+
+
                 break;
         }
         case 6:
         {
-                listarNombreMasApellido();
+                const filtrados = filtrarListado();
+                console.log("FILTRADOS");
+                console.table(filtrados);
+                break;
+        }
+        case 10:
+        {
+                alert("GRACIAS");
                 break;
         }
         default:{
@@ -79,36 +109,78 @@
 
  function agregarUsuario()
  {      
-     let id=1;
-     if(usuarios.length>0)
-     {
-        id=usuarios[usuarios.length-1].id+1;
-     }
-     
-     let nombre=prompt("ingrese un nombre");
-     let apellido = prompt("ingrese un apellido");
-     let usuario = new Usuario(id, nombre, apellido);
-
+     let nombre= prompt("Ingrese el nombre del usuario");
+     let apellido = prompt("Ingrese el apellido del usuario");
+     let usuario = new Usuario(getId(),nombre,apellido);
      usuarios.push(usuario);
+
+     console.log("USUARIO AGREGADO:")
+     console.table(usuarios);
  }
 
+ function getId()
+ {
+     if(usuarios.length===0)
+     {
+         return 1;
+     }
+     else
+     {
+        const ultimo = usuarios[usuarios.length-1];
+        return ultimo.id+1;
+     }
+ }
+
+ 
 
  function listarUsuarios()
  {
-    
+       console.log("LISTAR USUARIOS:");
+       usuarios.forEach((usuario)=>{
+            console.log(usuario.nombre+" "+usuario.apellido);
+       });
  }
 
  function eliminarUsuario(){
 
-    
+    const id = Number(prompt("Ingrese el id del que quiere borrar"));
+
+    const esta = usuarios.some((usuario)=>usuario.id===id);
+
+    if(esta)
+    {   
+        const soloIds = usuarios.map((usuario)=>usuario.id)
+        
+
+        let indice = soloIds.indexOf(id);
+
+        usuarios.splice(indice,1);
+        alert("Usuario borrado");
+
+       console.log("BORRADO");
+       console.table(usuarios);
+
+
+    }
+    else
+    {
+        alert("Usuario no encontrado");
+    }
     
 
  }
  
  function buscarUsuario()
  {
+    let idABuscar = Number(prompt("Ingrese el id del usuario:"));
     
-
+    return usuarios.find((usuario)=>idABuscar===usuario.id);
+                          /*
+                            Función de comparación
+                            el retorno de la función es un booleano
+                            y se puede formar de manera simple o 
+                            compuesta (usando && ó ||)
+                          */
  }
 
 
@@ -119,7 +191,17 @@
  }
 
 
- function listarNombreMasApellido()
+ function filtrarListado()
  {
+    let abuscar = prompt("Ingrese un texto para buscar");
+
+    return usuarios.filter((usuario)=>
     
+       usuario.nombre.indexOf(abuscar)!==-1 ||
+       usuario.apellido.indexOf(abuscar)!==-1
+
+    )
+
+    
+
  }
