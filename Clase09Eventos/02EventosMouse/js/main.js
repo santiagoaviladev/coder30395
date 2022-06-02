d672999b54154db2dee1b22ae958729a426d348d
Santiago Avila <xavier0823@gmail.com>
2022-06-01 20:13:19 -0500

    Eventos Todo

--- Clase09Eventos/02EventosMouse/js/main.js
+++ Clase09Eventos/02EventosMouse/js/main.js
@@ -2,9 +2,24 @@
   const boton1 = document.getElementById("miBoton1");
 
  //mouseover
+  boton1.addEventListener("mouseover", ()=>{
+    boton1.setAttribute("style", "background-color:green");
+  })
+
+  //mouseout
+  boton1.addEventListener("mouseout", ()=>{
+    boton1.setAttribute("style", "none");
+  })
   
 
  //mousedown
+ const boton2 = document.getElementById("miBoton2");
+ boton2.addEventListener("mousedown", ()=>{
+  document.body.setAttribute("style", "background-color:red");
+})
 
 
  //mouseup
+ boton2.addEventListener("mouseup", ()=>{
+  document.body.setAttribute("style", "none");
+})
--- Clase09Eventos/03EventosTeclado/js/main.js
+++ Clase09Eventos/03EventosTeclado/js/main.js
@@ -1,14 +1,54 @@
 
-
+const input2 = document.getElementById("input2");
+ 
  //onchange
+ input2.addEventListener("change", (event)=>{
+
+    const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    const valorTxt = input2.value;
+    const found = valorTxt.match(regex);
+
+    if(found)
+    {
+        alert("EMAIL VALIDO")
+    }
+    else
+    {
+        alert("EMAIL INVALIDO");
+    }
+
+
+ })
 
 
  //keypress
 
 
- //keyup 
-
-
  //keydown 
+ const input1 = document.getElementById("input1");
+ input1.addEventListener("keydown", ()=>{
+    document.body.setAttribute("style", "background-color:red");
+   
+ })
+
+
+ //keyup
+ input1.addEventListener("keyup", (event)=>{
+
+    const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    const valorTxt = input1.value;
+    const found = valorTxt.match(regex);
+
+    if(found)
+    {
+        alert("CADENA VALIDA")
+    }
+    else
+    {
+        alert("CADENA INVALIDA");
+    }
+
+
+ })
 
 
