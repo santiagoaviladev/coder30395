b05a0a58b2d8bec4287b3865ddd7444f6cd17411
Santiago Avila <xavier0823@gmail.com>
2022-06-06 18:14:54 -0500

    storage

---
+++ Clase10Storage/00EjercicioRegistro/js/main.js
@@ -0,0 +1,21 @@
+/*Realice las funciones para los formularios de registro y login: 
+
+Para el registro: 
+* Cree una clase usuario que le sirva como estructura para manejar la información 
+* Valide que todos los campos estén diligenciados 
+* valide que los campos de contraseña y confirmación coincidan
+* Una vez el usuario tenga los campos completos, guardar la información del usuario en el local storage. 
+
+
+Para el login (2)
+* Valide que los campos sean completados
+* Una vez se de click en login, valide con la información existente en local storage
+* Si los datos de acceso son correctos, redirija al usuario a 'My Account' 
+
+
+Bonus: 
+Si una vez se carga la página hay una sesión activa, redirija al usuario a 'My Account' 
+
+
+
+*/
