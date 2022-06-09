9cacfcd9b0fa7d0eb736bcba8b58303c69318f6b
Santiago Avila <xavier0823@gmail.com>
2022-06-08 19:31:27 -0500

    Clase Workshop

---
+++ Clase11Workshop/js/classes/categoria.class.js
@@ -0,0 +1,6 @@
+class Categoria{
+    constructor(id, nombre){
+        this.id=id;
+        this.nombre=nombre;
+    }
+}
--- Clase11Workshop/js/main.js
+++ Clase11Workshop/js/main.js
@@ -1,4 +1,6 @@
 
+let categoriasSistema = [];
+
 initApp();
 
 
@@ -8,16 +10,83 @@ function initApp() {
     loadData();
 }
 
-function loadData()
-{
-   loadCategories();
+function loadData() {
+    loadCategories();
+    showCategories();
+    showProducts();
+    console.log(categoriasSistema);
 }
 
-function loadCategories()
-{
-    const categories = products.map((element)=>element.category)
-    const set = new Set(categories); 
-    const finalCategories = [...set];
-    console.log(finalCategories);
+function loadCategories() {
+    const categorias = products.map(element =>  element.category);
+    const categoriasSet = new Set(categorias);
+    const categoriasUnicas = [...categoriasSet];
+
+    categoriasSistema = categoriasUnicas.map(element => {
+        return {
+            id: element,
+            nombre: element,
+        }
+    })
 }
 
+function showCategories() {
+    const divCategorias = document.getElementById("categoryMenu");
+    categoriasSistema.forEach(element => {
+        const btn = document.createElement("button");
+        btn.classList.add("catBtn");
+        btn.innerText = element.nombre;
+
+        btn.addEventListener("click", () => {
+            showProducts(element.nombre);
+        })
+
+        divCategorias.appendChild(btn);
+    })
+
+}
+//Asignación por defecto: 
+//Cuando se invoca sin parámetro, asigna el valor por defecto
+function showProducts(categoryName="") {
+    
+    let productsToShow = products;
+
+    if(categoryName!=="")
+    {
+        productsToShow = products.filter(product => product.category === categoryName);
+    }
+
+    
+    const categoryNode = document.getElementById("categoryName");
+    categoryNode.innerText = categoryName.toUpperCase();
+
+    /*productList*/
+    const productList = document.getElementById("productList")
+    productList.innerHTML="";
+    productsToShow.forEach(product => {
+        const div = document.createElement("div");
+        div.classList.add("product");
+        div.innerHTML = `<img src="${product.image}">
+        <div class="productInfo">
+            <div class="productName">
+               ${product.title}
+            </div>
+            <br>
+            <span class="productPrice">
+                Price:<b> $${product.price} </b>
+            </span> /
+            <span class="productRate">
+                Rate: <b> ${product.rating.rate} </b>
+            </span>
+           
+        </div>
+        <button class="addToList">
+            Agregar al Carrito
+        </button>
+        `
+
+        productList.appendChild(div);
+    })
+
+
+}
