48e85b65e28a38ce234fbe17142cfa1f872668fa
Santiago Avila <xavier0823@gmail.com>
2022-06-15 19:12:34 -0500

    Sweet Alert

--- ClaseLibrerias13/01SWEETALERT/js/main.js
+++ ClaseLibrerias13/01SWEETALERT/js/main.js
@@ -1,8 +1,7 @@
 init();
 
 
-function init()
-{
+function init() {
     saludarConSweetAlert();
     alertSinBotonDeConfirmacion();
     alertConImagenes();
@@ -10,23 +9,87 @@ function init()
 
 }
 
-function saludarConSweetAlert()
-{
-   
-    
+function saludarConSweetAlert() {
+
+    const boton = document.getElementById("btnSweet");
+    boton.addEventListener("click", () => {
+
+        Swal.fire({
+            title: '¡Hola, a todos!',
+            text: 'Esto es un saludo con Sweet Alert',
+            icon: 'error',
+            confirmButtonText: '¡Está muy Piola!',
+        })
+
+
+    })
+
+
+
+
 }
 
-function alertSinBotonDeConfirmacion()
-{
-    
+function alertSinBotonDeConfirmacion() {
+   
+    const boton = document.getElementById("btnSweet2");
+    boton.addEventListener("click", () => {
+
+        Swal.fire({
+            title: '¡Hola, a todos!',
+            text: 'Esto es un saludo con Sweet Alert',
+            icon: 'success',
+            showConfirmButton: false,
+            timer: 2000,
+        })
+
+
+    })
 }
 
-function alertConImagenes()
-{
-   
+function alertConImagenes() {
+
+    const boton = document.getElementById("btnSweet3");
+    boton.addEventListener("click", () => {
+
+        Swal.fire({
+            title: '¡Hola, a todos!',
+            text: 'Esto es un saludo con Sweet Alert',
+            icon: 'success',
+            imageUrl: 'https://jw-webmagazine.com/wp-content/uploads/2021/09/howl003.jpeg', 
+            imageWidth: 200, 
+            background: 'aqua',
+        })
+
+
+    })
+
 }
 
-function alertConEventos()
-{
-   
+function alertConEventos() {
+
+    const boton = document.getElementById("btnSweet4");
+    boton.addEventListener("click", () => {
+
+        Swal.fire({
+            title: '¡Hola, a todos!',
+            text: 'Esto es un saludo con Sweet Alert',
+            icon: 'success',
+            imageUrl: 'https://jw-webmagazine.com/wp-content/uploads/2021/09/howl003.jpeg', 
+            imageWidth: 200, 
+            showCancelButton: true,
+            confirmButtonText: '¡VAMOS!', 
+            cancelButtonText: '¡NO QUIERO!',
+        }).then((resultado)=>{
+
+            if(resultado.isConfirmed){
+                alert("QUÉ BIEN");
+            }
+            else {
+                alert("QUÉ MAL");
+            }
+
+        })
+
+
+    })
 }
