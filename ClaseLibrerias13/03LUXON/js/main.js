98954d5898bd61c59b18db7dc113d45e599414f0
Santiago Avila <xavier0823@gmail.com>
2022-06-15 20:35:27 -0500

    luxon

--- ClaseLibrerias13/03LUXON/js/main.js
+++ ClaseLibrerias13/03LUXON/js/main.js
@@ -12,15 +12,25 @@ function init()
 
 function crearDateTimeLocal()
 {
+  const hoy = luxonDateTime.local(); 
+  console.log("HOY", hoy);
+
   
 }
 
 function crearDateTimeConArgumentos()
 {
-  
+  const otroDia = luxonDateTime.local(1992, 12, 14, 23, 0);
+  console.log("CUMPLEAÑOS JUAN", otroDia);
 }
 
 function crearDateAPartirDeUnObjeto()
 {
- 
+  const dt = luxonDateTime.fromObject(
+    {day: 4, month:3, year:1998},
+    {zone: 'America/Bogot'},
+  );
+
+  console.log("CUMPLEAÑOS NATALIA", dt);
+
 }
