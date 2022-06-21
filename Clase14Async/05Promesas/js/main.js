3440a127b26f279b125a8e398760a2e1f22f0c20
Santiago Avila <xavier0823@gmail.com>
2022-06-20 20:10:29 -0500

    Promises

--- Clase14Async/05Promesas/js/main.js
+++ Clase14Async/05Promesas/js/main.js
@@ -1,2 +1,24 @@
 
+   const promise = (res) => new Promise((resolve, reject)=>{
+      
+        if(res)
+        { 
+          resolve({status:'200', msg: 'ok', id:3})
+        }
+        else
+        {
+          reject("NO SE RESOLVIO LA PROMESA")
+        }
 
+   })
+
+   promise(true)
+   .then((res)=>console.log(res))
+   .catch((error)=>console.log(error))
+   .finally(()=>console.log("LO LOGRAMOS"))
+
+
+   promise(false)
+   .then((res)=>console.log(res))
+   .catch((error)=>console.log(error))
+   .finally(()=>console.log("NO LO LOGRAMOS"))
